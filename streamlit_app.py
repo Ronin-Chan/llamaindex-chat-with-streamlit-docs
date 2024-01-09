07fe19b37d0e87fa32902494252d937e89aa29a0
IT Intern (CiiLOCK) <it.intern@ciilock.com>
2024-01-09 15:17:47 +1100

    modify

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI as LlamaOpenAI
-from openai import OpenAI
+from llama_index.llms import OpenAI
+from openai import OpenAI as OriginalOpenAI
 from llama_index import SimpleDirectoryReader
 
 st.set_page_config(page_title="Chat with CiiLOCK chatbot", page_icon="⚙️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai_api_key = st.secrets.openai_key
-client = OpenAI(
+client = OriginalOpenAI(
         # This is the default and can be omitted
         api_key=openai_api_key,
     )
@@ -22,7 +22,7 @@ def load_data():
     with st.spinner(text="Loading and indexing the Streamlit docs - hang tight! This should take 1-2 minutes."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=LlamaOpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts - do not hallucinate features. If you do not know the answer, respond with \"Sorry, I don't know.\""))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts - do not hallucinate features. If you do not know the answer, respond with \"Sorry, I don't know.\""))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
